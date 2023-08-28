17123136c59ef65d91b39b418cd6ed6d8486b76a
Tharindu37 <tharindut520@gmail.com>
2023-08-29 01:03:50 +0530

    convert from numeric to plaintext

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const plaintextBuffer = Buffer.from(plaintext, 'utf-8');
 const plaintextNumeric = BigInt('0x' + plaintextBuffer.toString('hex'));
 
 console.log(plaintextNumeric); // Output the BigInt representation
+
+
+const plaintextHex = plaintextNumeric.toString(16); // Convert the BigInt back to hexadecimal string
+const plaintextBufferRestored = Buffer.from(plaintextHex, 'hex'); // Convert the hexadecimal string back to Buffer
+const restoredPlaintext = plaintextBufferRestored.toString('utf-8'); // Convert the Buffer back to plaintext string
+
+console.log(restoredPlaintext); // Output: Hello, world!
