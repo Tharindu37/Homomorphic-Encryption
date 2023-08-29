0a50ed1b212ef94c247d63282fe204b73b5cd204
Tharindu37 <tharindut520@gmail.com>
2023-08-29 12:59:35 +0530

    add the example

---
+++ test.js
@@ -0,0 +1,21 @@
+const paillierBigint  = require('paillier-bigint');
+
+async function paillierTest () {
+    // (asynchronous) creation of a random private, public key pair for the Paillier cryptosystem
+    const { publicKey, privateKey } = await paillierBigint.generateRandomKeys(3072)
+
+    const ages = [12n, 5n, 30n, 45n];
+    const encryptedAges = [];
+
+    for(i=0;i<ages.length;i++){
+        encryptedAges.push(publicKey.encrypt(ages[i]));
+    }
+  
+    const encryptedSum = encryptedAges.reduce((accumulator, age) => publicKey.addition(accumulator, age));
+    const numIndividuals = BigInt(encryptedAges.length);
+    console.log(numIndividuals)
+    const decryptSum=privateKey.decrypt(encryptedSum);
+    console.log(decryptSum)
+    console.log(decryptSum/numIndividuals)
+  }
+  paillierTest()
